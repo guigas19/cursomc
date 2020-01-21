df3e264b8845fd6208cdacdf76392676f47b4f75
guigas19 <guilhermecaetano19@gmail.com>
2020-01-21 11:43:11 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/guilherme/cursomc/resources/ClienteResource.java
@@ -0,0 +1,33 @@
+package br.com.guilherme.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.guilherme.cursomc.domain.Categoria;
+import br.com.guilherme.cursomc.domain.Cliente;
+import br.com.guilherme.cursomc.services.CategoriaService;
+import br.com.guilherme.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+
+}
---
+++ src/main/java/br/com/guilherme/cursomc/services/ClienteService.java
@@ -0,0 +1,29 @@
+package br.com.guilherme.cursomc.services;
+
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.guilherme.cursomc.domain.Categoria;
+import br.com.guilherme.cursomc.domain.Cliente;
+import br.com.guilherme.cursomc.repository.CategoriaRepository;
+import br.com.guilherme.cursomc.repository.ClienteRepository;
+import br.com.guilherme.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	
+
+}
