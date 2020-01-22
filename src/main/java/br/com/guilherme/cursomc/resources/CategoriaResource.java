9a7fdd9c0e0b36d5cc59c2c92b3138abb1078ecd
guigas19 <guilhermecaetano19@gmail.com>
2020-01-22 10:38:57 -0300

    Atualizado categoria com PUT, outros pequenos ajustes

--- src/main/java/br/com/guilherme/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/guilherme/cursomc/resources/CategoriaResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +24,9 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
@@ -35,6 +36,14 @@ public class CategoriaResource {
 		categoria = service.insert(categoria);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody Categoria categoria, @PathVariable Integer id){
+		categoria.setId(id);
+		service.update(categoria);
+		return ResponseEntity.noContent().build();
+		
 	}
 
 }
--- src/main/java/br/com/guilherme/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/guilherme/cursomc/services/CategoriaService.java
@@ -16,7 +16,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
@@ -25,6 +25,12 @@ public class CategoriaService {
 	public Categoria insert(Categoria categoria) {
 		categoria.setId(null);
 		return repository.save(categoria);		
+	}
+
+	public Categoria update(Categoria categoria) {
+		find(categoria.getId());
+		return repository.save(categoria);
+		
 	}
 	
 	
