04cc772b88fd2bbd9fc0cf8ed6cf07ed75ec5787
guigas19 <guilhermecaetano19@gmail.com>
2020-01-24 10:36:21 -0300

    validacao customizada para email não repitido para email do Cliente

--- src/main/java/br/com/guilherme/cursomc/repository/ClienteRepository.java
+++ src/main/java/br/com/guilherme/cursomc/repository/ClienteRepository.java
@@ -1,9 +1,14 @@
 package br.com.guilherme.cursomc.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.guilherme.cursomc.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/br/com/guilherme/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/guilherme/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.guilherme.cursomc.domain.Cliente;
 import br.com.guilherme.cursomc.domain.enums.TipoCliente;
 import br.com.guilherme.cursomc.dto.ClienteNewDTO;
+import br.com.guilherme.cursomc.repository.ClienteRepository;
 import br.com.guilherme.cursomc.resources.exception.FieldMessage;
 import br.com.guilherme.cursomc.services.validation.utils.Br;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !Br.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente!"));
 		}
 
 		for (FieldMessage e : list) {
