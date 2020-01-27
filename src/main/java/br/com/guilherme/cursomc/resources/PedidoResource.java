56a4ac8540a145dc8404d7c75858550268e3b494
guigas19 <guilhermecaetano19@gmail.com>
2020-01-27 16:59:17 -0300

    Inserindo Pedido

--- src/main/java/br/com/guilherme/cursomc/resources/PedidoResource.java
+++ src/main/java/br/com/guilherme/cursomc/resources/PedidoResource.java
@@ -1,11 +1,18 @@
 package br.com.guilherme.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.guilherme.cursomc.domain.Pedido;
 import br.com.guilherme.cursomc.services.PedidoService;
@@ -23,6 +30,13 @@ public class PedidoResource {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@PostMapping	
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido pedido){		
+		pedido = service.insert(pedido);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(pedido.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/br/com/guilherme/cursomc/services/PedidoService.java
+++ src/main/java/br/com/guilherme/cursomc/services/PedidoService.java
@@ -1,12 +1,18 @@
 package br.com.guilherme.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.guilherme.cursomc.domain.ItemPedido;
+import br.com.guilherme.cursomc.domain.PagamentoComBoleto;
 import br.com.guilherme.cursomc.domain.Pedido;
+import br.com.guilherme.cursomc.domain.enums.EstadoPagamento;
+import br.com.guilherme.cursomc.repository.ItemPedidoRepository;
+import br.com.guilherme.cursomc.repository.PagamentoRepository;
 import br.com.guilherme.cursomc.repository.PedidoRepository;
 import br.com.guilherme.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -15,11 +21,42 @@ public class PedidoService {
 	
 	@Autowired
 	private PedidoRepository repository;
+	@Autowired
+	private BoletoService boletoService;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ProdutoService produtoService;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	@Transactional
+	public Pedido insert(Pedido pedido) {
+		pedido.setId(null);
+		pedido.setInstante(new Date());
+		pedido.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		pedido.getPagamento().setPedido(pedido);
+		if (pedido.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) pedido.getPagamento();
+			boletoService.preenchimentoPagamentoComBoleto(pagto, pedido.getInstante());
+		}
+		pedido = repository.save(pedido);
+		pagamentoRepository.save(pedido.getPagamento());
+		
+		for (ItemPedido item : pedido.getItens()) {
+			item.setDesconto(0.0);
+			item.setProduto(produtoService.find(item.getProduto().getId()));
+			item.setPreco(item.getProduto().getPreco());
+			item.setPedido(pedido);
+		}
+		itemPedidoRepository.saveAll(pedido.getItens());
+		return pedido;
 	}
 	
 	
